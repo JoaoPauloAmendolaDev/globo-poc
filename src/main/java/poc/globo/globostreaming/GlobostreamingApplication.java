b555c2a3b436bbe174cb4adfa6e4b0614b1a41f0
Joao Paulo <joaoa@id.uff.br>
2026-01-10 19:15:24 -0300

    initial commit

---
+++ src/main/java/poc/globo/globostreaming/GlobostreamingApplication.java
@@ -0,0 +1,13 @@
+package poc.globo.globostreaming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlobostreamingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GlobostreamingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/poc/globo/globostreaming/GlobostreamingApplicationTests.java
@@ -0,0 +1,13 @@
+package poc.globo.globostreaming;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlobostreamingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
